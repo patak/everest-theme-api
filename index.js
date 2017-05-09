5b45af42aa94eacfb8f7a11ad1b44b789c7b3387
patak <pataky@hotmail.fr>
2017-05-09 16:13:09 +0200

    feat: download html template with replaced values

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 var express = require('express');
+var fs = require('fs');
+var replace = require('stream-replace');
+
 var app = express();
 
 app.get('/', function (req, res) {
@@ -6,9 +9,16 @@ app.get('/', function (req, res) {
 });
 
 app.get('/download', function(req, res){
+
   var file = __dirname + '/theme/app-theme.example.html';
 
-  res.download(file, 'app-theme.html'); // Set disposition and send it.
+  var rstream = fs.createReadStream(file);
+
+  res.writeHead(200, {"Content-Type" : "application/force-download", "Content-Disposition" : "filename='app-theme.html'"});
+
+  rstream.pipe(replace(/#darkPrimaryValue/g,'#FFFF' ))
+  .pipe(res);
+
 });
 
 app.listen(3000, function () {
