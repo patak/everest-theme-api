ba76f0b2d647a4f5e0c74a660f31c12f9d9f7527
patak <pataky@hotmail.fr>
2017-06-19 01:05:48 +0200

    feat: manage upload

--- index.js
+++ index.js
@@ -2,11 +2,12 @@ var express = require('express');
 var fs = require('fs');
 var replace = require('stream-replace');
 var bodyParser = require('body-parser');
-
+var formidable = require('formidable');
+var path = require('path');
 
 var app = express();
 
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
@@ -16,12 +17,12 @@ app.get('/', function (req, res) {
   res.send('Hello World!');
 });
 
-app.post('/download', bodyParser.urlencoded(true), function(req, res){
-  var file = __dirname + '/theme/app-theme.example.html';
+app.post('/download', bodyParser.urlencoded(true), function (req, res) {
+  var file = path.join(__dirname, '/theme/app-theme.example.html');
   var rstream = fs.createReadStream(file);
 
   var obj = req.body;
-  res.writeHead(200, {"Content-Type" : "application/force-download", "Content-Disposition" : "filename='app-theme.html'"});
+  res.writeHead(200, { "Content-Type": "application/force-download", "Content-Disposition": "filename='app-theme.html'" });
 
   for (var property in obj) {
     if (obj.hasOwnProperty(property)) {
@@ -30,8 +31,30 @@ app.post('/download', bodyParser.urlencoded(true), function(req, res){
     }
   }
   rstream.pipe(replace(/(#.*#)+/gmi, "inherit"))
-  .pipe(res);
+    .pipe(res);
 
+});
+
+app.post('/upload', function (req, res) {
+  var form = new formidable.IncomingForm();
+
+  form.multiples = true;
+
+  form.uploadDir = path.join(__dirname, '/uploads');
+
+  form.on('file', function (field, file) {
+    fs.rename(file.path, path.join(form.uploadDir, file.name));
+  });
+
+  form.on('error', function (err) {
+    console.log('An error has occured: \n' + err);
+  });
+
+  form.on('end', function () {
+    res.status(200).end('success');
+  });
+
+  form.parse(req);
 });
 
 app.listen(3000, function () {
