a1691fd0edf6440b2cd60328721f166f94c3b994
patak <pataky@hotmail.fr>
2017-07-05 15:08:00 +0200

    [fix] - remove Formidable & add Multer (2)

--- index.js
+++ index.js
@@ -83,72 +83,37 @@ app.post('/download', bodyParser.urlencoded(true), function (req, res) {
 });
 
 app.post('/upload', function (req, res) {
-  var form = new formidable.IncomingForm();
-  var newPath = null;
-  var minifyPath = null;
-  var fileBaseName = null;
-  var newFileName = null;
-  var dataImage = null;
+    var upload = multer({
+        storage: storage
+    }).any();
 
-  form.multiples = true;
-  form.uploadDir = path.join(__dirname, '/uploads');
+    upload(req, res, function (err) {
 
+        var bitmap = fs.readFileSync('./uploads/' + req.files[0].filename).toString('hex', 0, 4);
+        if (!checkMagicNumbers(bitmap)) {
+            fs.unlinkSync('./uploads/' + req.files[0].filename);
+            res.status(403).end('File is not valid');
+        }
 
-  form.on('fileBegin', function (field, file) {
-    if (file.type != 'image/png' && file.type != 'image/jpg' && file.type != 'image/jpeg') {
-      form.emit('error', 'File not Valid');
-      return;
-    }
-  });
+        var minifyOriginalImage = new Promise(function (resolve, reject) {
+            Jimp.read(filePath).then(function (image) {
+                image.quality(10)
+                    .write(minifyPath, resolve);
+            }).catch(function (err) {
+                console.error(err);
+                return res.status(400).end("Error during minify process");
+            });
+        });
 
-  form.on('file', function (field, file) {
-    fileBaseName = path.basename(file.name, path.extname(file.name));
-    fileBaseName = randomstring.generate(7);
+        minifyOriginalImage.then(function () {
+            dataImage = base64Img.base64Sync(minifyPath);
+            res.status(200).json({
+                imageName: newFileName,
+                imageBase64: dataImage
+            });
+        });
 
-    newFileName = fileBaseName + path.extname(file.name);
-
-    minifyPath = path.join(form.uploadDir, "m_" + newFileName);
-    newPath = path.join(form.uploadDir, newFileName);
-
-    fs.rename(file.path, newPath);
-  });
-
-  form.on('error', function (err) {
-    console.log('An error has occured: \n' + err);
-    res.header('Connection', 'close');
-    return res.status(403).end(err);
-  });
-
-  form.parse(req, function (err) {
-
-    if (err)
-      return err;
-
-    /**
-    * Write new minify image
-    */
-
-    var minifyOriginalImage = new Promise(function (resolve, reject) {
-      Jimp.read(newPath).then(function (image) {
-        image.quality(10)
-          .write(minifyPath, resolve);
-      }).catch(function (err) {
-        console.error(err);
-        return res.status(403).end("Incorrect File");
-      });
-    });
-
-    minifyOriginalImage.then(function () {
-      dataImage = base64Img.base64Sync(minifyPath);
-      res.status(200).json({
-        imageName: newFileName,
-        imageBase64: dataImage
-      });
-    });
-
-    return;
-  });
-
+    })
 });
 
 
